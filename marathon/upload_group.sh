c22712ba31a0fba822b38b40c3eea523b5f84d2c
Wang Qiang <wangqiang8511@gmail.com>
2016-02-21 17:28:23 +0800

    Fixed the hdfs marathon config error.

--- marathon/upload_group.sh
+++ marathon/upload_group.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Upload artifacts to marathon
-MARATHON_MASTER=${MARATHON_MASTER:-"http://127.0.0.1:8080"}
+MARATHON_MASTER=${MARATHON_MASTER:-"http://127.0.0.1:8081"}
 
 curl -include -XPUT "$MARATHON_MASTER/v2/groups" -d @$1
